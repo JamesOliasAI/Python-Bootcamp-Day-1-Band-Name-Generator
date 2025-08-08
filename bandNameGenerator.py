231b1d33f5e79a22dc1e6b1d9c09632fbcd56b1c
James Olias <jamesmolias18@gmail.com>
2025-08-08 01:16:15 +0100

    Create bandNameGenerator.py

---
+++ bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Hey, Let's generate your band name!")
+city = input("What city did you grow up in?")
+pet_name = input("What is your pets name?")
+print("Your band name should be " + city + " " + pet_name + "!")
